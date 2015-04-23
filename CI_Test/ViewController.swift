622f90e4902bbc2126831b5e8396feeadb09ec89
acavanaghww <acavanagh@weddingwire.com>
2015-04-23 11:22:24 -0400

    Initial Commit

---
+++ CI_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CI_Test
+//
+//  Created by Andrew Cavanagh on 4/23/15.
+//  Copyright (c) 2015 WeddingWire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
